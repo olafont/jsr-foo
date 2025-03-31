990fd5eda8421d01c5659dca5e29928e278da7a3
Olivier <olafont@gmail.com>
2025-03-31 17:23:58 -0400

    first commit

---
+++ foo.ts
@@ -0,0 +1,12 @@
+// foo.ts
+/**
+ * Greet a person.
+ * @param name The name of the person to greet.
+ */
+export function greet(name: string) {
+    console.log(`Hello, ${name}!`);
+  }
+
+  export function bye(name: string) {
+    console.log(`Bye, ${name}!`);
+  }
---
+++ mod.ts
@@ -0,0 +1,17 @@
+// mod.ts
+/**
+ * A module providing a function to greet people.
+ *
+ * @example
+ * ```ts
+ * import { greet } from "@olafont/foo";
+ * import {bye} from "@olafont/foo";
+ *
+ * greet("Yoda");
+ * bye("Luke");
+ * ```
+ *
+ * @module
+ */
+
+export * from "./foo.ts";
